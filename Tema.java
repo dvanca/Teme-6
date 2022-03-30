4041fba73488572d68ef5a59ffb99d520312fce3
dvanca <102247365+dvanca@users.noreply.github.com>
2022-03-30 14:33:08 +0300

    Update Tema.java

--- Tema.java
+++ Tema.java
@@ -2,5 +2,7 @@ public class Tema{
 	public static void main(String[] args){
 		System.out.println("Start !!! !!!!");
 		System.out.println("Start !!! !!!!");
+		System.out.println("Start !!! !!!!");
+		System.out.println("Start !!! !!!!");
 	}
 }
