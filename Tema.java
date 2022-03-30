0d10fbb24e1b65d834b83d51de69c2b0a4e0ba00
dvanca <vanca37@gmail.com>
2022-03-30 15:12:15 +0300

    prima problema

--- Tema.java
+++ Tema.java
@@ -1,8 +1,15 @@
+import java.sql.SQLOutput;
+
 public class Tema{
 	public static void main(String[] args){
-		System.out.println("Start !!! !!!!");
-		System.out.println("Start !!! !!!!");
-		System.out.println("Start !!! !!!!");
-		System.out.println("Start !!! !!!!");
+		System.out.println(rezultatProdus(new int[]{1,2,3,4,5,6}));
+	}
+
+	public static int rezultatProdus(int[]a){
+		int rezultatInmultire=1;
+		for(int c : a){
+			rezultatInmultire = rezultatInmultire *c;
+		}
+		return rezultatInmultire;
 	}
 }
